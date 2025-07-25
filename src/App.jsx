c515e91d2fe1ffe28fa83d6a7d21da4d1a88f6e4
Rasagnachithaluru <you@example.com>
2025-07-25 12:30:32 +0530

    17

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/CICD">
+    <Router basename="/S109">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
